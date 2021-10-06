38acfe064600ff7cd87bbd5f169a186e01e3e1be
Renato Gonçalves <renatoslip@hotmail.com>
2021-10-05 22:04:01 -0300

    initial commit - Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Comprar o pão na padaria", done: false },
+    { id: 2, name: "Comprar bolo na padaria", done: false },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área de adicionar uma nova tarefa */}
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
+
+        {/* Lista de tarefas */}
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
