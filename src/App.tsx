70f71b3f5f90de7389118968eb2e4f163a925add
Renato Gonçalves <renatoslip@hotmail.com>
2021-10-05 23:42:55 -0300

    show the list of items

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, name: "Comprar o pão na padaria", done: false },
-    { id: 2, name: "Comprar bolo na padaria", done: false },
+    { id: 2, name: "Comprar bolo na padaria", done: true },
   ]);
   return (
     <C.Container>
@@ -13,11 +14,11 @@ const App = () => {
         <C.Header>Lista de Tarefas</C.Header>
 
         {/* Área de adicionar uma nova tarefa */}
-        {list.map((item, index) => (
-          <div>{item.name}</div>
-        ))}
 
         {/* Lista de tarefas */}
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
   );
